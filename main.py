c8b0d4e0771d4236519b0a71707b54354c8563fb
M. Onur Cirit <onur.cirit@tttech-auto.com>
2021-12-14 14:22:54 +0300

    template is created

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+
+
+class Register:
+    
+    registered_users = {}
+
+    def __init__(self, input_files):
+        # parse JSON files
+        # create a union of user data
+        # implement error checks
+        # optional: run json parsers in parallel
+        pass
+
+    # overloaded methods
+    def __len__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def get_name(self, key):
+        pass
+
+    def get_IP(self, key):
+        pass
+
+    def get_devices(self, key):
+        pass
+
+    def set_name(self, key, name):
+        pass
+
+    def set_IP(self, key, IP):
+        pass
+
+    def set_devices(self, key, devices):
+        pass
+
+if __name__ == "__main__":
+    print("this")
